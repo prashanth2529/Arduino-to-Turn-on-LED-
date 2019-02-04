6fef186c564a1f525816b803852e3bbf07d5e01b
prashanth2529 <47311332+prashanth2529@users.noreply.github.com>
2019-02-04 12:46:32 +0800

    Add files via upload
    This program is Used to Blink an LED with a 1 Second Delay. the Pin Numbers can be changed, make sure you Connect the same pin
    Arduino which you entered in the Program.

---
+++ blinkled.ino
@@ -0,0 +1,19 @@
+//Created By Prashanth.S 
+//Date: 27/3/2018
+// The Hardware used is Arduino Uno where the pin no 5 is connected to an 
+// an LED through a 220ohm or 100 ohm resistor.
+
+const int ledpin=5;        // Pin 5 of Arduino  is connected to LED through Resistor
+void setup()               // Set up function for setting the Output pin
+{
+   Serial.begin(9600);
+   pinMode(ledpin,OUTPUT);  // Describes the ledpin is an Output 
+  
+}
+void loop()                    // Loop Function for Keep repeating 
+{
+    digitalWrite(ledpin,HIGH);   // Turn On the LED 
+    delay(1000);                 // 1 sec delay 
+    digitalWrite(ledpin,LOW);    // Turn off the LED 
+    delay(1000);                 // 1 Sec Delay
+}
